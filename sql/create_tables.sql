beab8b04ec2820ec57f1b3a59eaee708f35c54b0
Jokeren <robinho364@gmail.com>
2018-11-10 21:11:04 -0600

    Add first draft of table definitions

---
+++ sql/create_tables.sql
@@ -0,0 +1,50 @@
+CREATE TABLE Movie (
+  movieId CHAR(9),
+  title VARCHAR(100) NOT NULL,
+  releaseYear INTEGER NOT NULL,
+  isAdult BOOLEAN DEFAULT FALSE,
+  runtimeMinutes INTEGER,
+  rating numeric(5, 2),
+  numVotes INTEGER,
+  PRIMARY KEY (movieId)
+);
+
+CREATE TABLE Alias (
+  movieId CHAR(9),
+  title VARCHAR(100) NOT NULL,
+  isOriginal BOOLEAN DEFAULT FALSE,
+  region VARCHAR(50),
+  PRIMARY KEY (movieId, isOriginal, region),
+  FOREIGN KEY (movieId) REFERENCES Movie(movieId)
+);
+
+CREATE TABLE Genre (
+  movieId CHAR(9),
+  type VARCHAR(50) NOT NULL,
+  PRIMARY KEY (movieId, type)
+);
+
+CREATE TABLE Person (
+  personId CHAR(9),
+  name VARCHAR(100) NOT NULL,
+  birthYear INTEGER,
+  deathYear INTEGER,
+  age INTEGER,
+  PRIMARY KEY (personId)
+);
+
+CREATE TABLE Role (
+  roleId CHAR(9),
+  type VARCHAR(50) NOT NULL,
+  PRIMARY KEY (roleId)
+);
+
+CREATE TABLE Assign (
+  movieId CHAR(9),
+  personId CHAR(9),
+  roleId CHAR(9),
+  PRIMARY KEY (movieId, personId, roleId),
+  FOREIGN KEY (movieId) REFERENCES Movie(movieId),
+  FOREIGN KEY (personId) REFERENCES Person(personId),
+  FOREIGN KEY (roleId) REFERENCES Role(roleId)
+);

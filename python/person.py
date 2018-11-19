9c4c811dcaa7a9b7d297ffc4ec3074ebcc75bed2
丁哲禹 <dingzheyu@teiakiraus-MacBook-puro.local>
2018-11-19 10:24:51 -0600

    Revise a mistake in person.py

--- python/person.py
+++ python/person.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 import numpy as np
+import pandas as pd
 
 Person = namedtuple("Person", ["personId", "name", "birthYear", "deathYear", "age"])
 person_columns = ["personId", "name", "birthYear", "deathYear", "age"]
@@ -13,26 +14,28 @@ def clean_person(tsv_principals):
     clean_birthYear = np.array(tsv_principals['birthYear'])
     clean_deathYear = np.array(tsv_principals['deathYear'])
     for i in range(len(clean_birthYear)):
-        if clean_deathYear[i] == r'\N':
-            clean_deathYear[i] = ''
+        if clean_birthYear[i] == r'\N':
+            clean_birthYear[i] = ''
     for i in range(len(clean_deathYear)):
         if clean_deathYear[i] == r'\N':
             clean_deathYear[i] = ''
     tsv_principals['birthYear'] = clean_birthYear
     tsv_principals['deathYear'] = clean_deathYear
-    
+
     for row in tsv_principals.itertuples():
         if row.birthYear == r'':
             age = ''
         if row.birthYear != r'' and row.deathYear == r'':
             age = 2018 - int(row.birthYear)
-        if row.birthYear != r'' and row.deathYear != r'':  
-            age = int(row.deathYear)- int(row.birthYear)
-        person = Person(personId = row.nconst,
-                        name = row.primaryName,
-                        birthYear = row.birthYear,
-                        deathYear = row.deathYear,
-                        age = age)
-        
+        if row.birthYear != r'' and row.deathYear != r'':
+            age = int(row.deathYear) - int(row.birthYear)
+        person = Person(personId=row.nconst,
+                        name=row.primaryName,
+                        birthYear=row.birthYear,
+                        deathYear=row.deathYear,
+                        age=age)
+
         rows.append(person._asdict())
     return pd.DataFrame(rows, columns=person_columns)
+
+

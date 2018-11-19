e80ac44664d046a7fd2c0098900ea20dd0a2e222
丁哲禹 <dingzheyu@teiakirbookpuro.dyndns.rice.edu>
2018-11-18 23:43:37 -0600

    Table Role finished

--- python/role.py
+++ python/role.py
@@ -1,10 +1,20 @@
 from collections import namedtuple
 
+role_columns = ["roleId", "type"]
 Role = namedtuple("Role", ["roleId", "type"])
-
 
 def clean_role(tsv_principals):
     """
     :param tsv_principals:
     """
-    pass
+    rows = []
+    num = 1
+    distinct = tsv_principals['category'].drop_duplicates()
+    for row in distinct:
+        roleId = 'r'+str(num)
+        role = Role(roleId = roleId,
+                    type = row)
+        
+        rows.append(role._asdict())
+        num +=1    
+    return pd.DataFrame(rows, columns=role_columns)

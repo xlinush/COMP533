da8eff39d1766adbd40ae5cb6339260c8d36b313
BenDing96 <35756863+BenDing96@users.noreply.github.com>
2018-11-20 10:51:01 -0600

    Update genre.py

--- python/genre.py
+++ python/genre.py
@@ -1,29 +1,12 @@
 from collections import namedtuple
-import pandas as pd
-import numpy as np
 
-genre_columns = ["genreId", "genre"]
-Genre = namedtuple("Genre", ["genreId", "genre"])
+Genre = namedtuple("Genre", ["movieId", "type"])
+
 
 def clean_genre(tsv_movies):
     """
     :param tsv_movies:
     """
-    rows = []
-    genres = []
-    for couple in np.array(tsv_movies['genres']):
-        for genre in couple.split(','):
-            genres.append(genre)
-    genres = set(genres)
-    num = 0
-    for genre in genres:
-        if genre == r'\N':
-            continue
-        num += 1
-        genreId = 'g' + str(num)
-        genre = Genre(genreId=genreId,
-                      genre=genre)
-        rows.append(genre._asdict())
-    return pd.DataFrame(rows, columns=genre_columns)
+    pass
 
 
